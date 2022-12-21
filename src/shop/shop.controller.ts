a4d9a8b8c5edbb1d885b53a6e4d00124ba3c9801
kewool <kkttyy324@gmail.com>
2022-12-21 14:37:24 +0900

    rating

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -79,7 +79,7 @@ export class ShopController {
     const shopReview = await this.shopService.getShopReview(shop_uuid);
     const reviewCount = await this.shopService.getShopReviewCount(shop_uuid);
     let ratingSum = 0;
-    for (const i of shopReview) {
+    for await (const i of shopReview) {
       ratingSum += i.rating;
     }
     shop.shop_rating = ratingSum / reviewCount;
